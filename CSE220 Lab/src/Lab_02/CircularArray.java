6a8bb7d8a4bdd40d95a90ec591011b32cabaa0fd
alelola <alelola@users.noreply.github.com>
2020-01-29 11:07:50 +0600

    lab2solve

---
+++ CSE220 Lab/src/Lab_02/CircularArray.java
@@ -0,0 +1,217 @@
+package Lab_02;
+
+public class CircularArray {
+
+    private int start;
+    private int size;
+    private Object[] cir;
+
+    /*
+     * if Object [] lin = {10, 20, 30, 40, null}
+     * then, CircularArray(lin, 2, 4) will generate
+     * Object [] cir = {40, null, 10, 20, 30}
+     */
+    public CircularArray(Object[] lin, int st, int sz) {
+        start = st;
+        size = sz;
+        cir = new Object[lin.length];
+        int str = st;
+        for (int i = 0; i < size; i++) {
+            cir[str] = lin[i];
+            str++;
+            //System.out.println(str);
+            if (str >= lin.length) {
+                str = 0;
+            }
+        }
+    }
+
+    //Prints from index --> 0 to cir.length-1
+    public void printFullLinear() {
+        for (int i = 0; i < cir.length; i++) {
+            //if(cir[i]!=null){
+            System.out.print(cir[i] + " ");
+            //}
+        }
+        System.out.println();
+    }
+
+    // Starts Printing from index start. Prints a total of size elements
+    public void printForward() {
+        int str = start;
+        for (int i = 0; i < size; i++) {
+            System.out.print(cir[str] + " ");
+            str++;
+            if (str >= cir.length) {
+                str = 0;
+            }
+        }
+        System.out.println();
+    }
+
+    public void printBackward() {
+        int last=(start+size-1)%cir.length;
+        for(int i=0; i<size; i++){
+            System.out.print(cir[last]+" ");
+            last--;
+            if(last<0){
+                last=cir.length-1;
+            }
+        }
+        System.out.println();
+    }
+
+    // With no null cells
+    public void linearize() {
+        Object[] aba=new Object[size];
+        int st=start;
+        for (int i = 0; i < size; i++) {
+
+            if(cir[st]!=null){
+                aba[i]=cir[st];
+            }
+            st++;
+            if(st>=cir.length){
+                st=0;
+            }
+
+        }
+        cir=aba;
+    }
+
+    // Do not change the Start index
+    public void resizeStartUnchanged(int newcapacity) {
+        Object aba[]=new Object[newcapacity];
+        int st1=start, st2=start;
+
+        for(int i=0; i<size; i++){
+            aba[st1]=cir[st2];
+            st1++;
+            st2++;
+            if(st1>=aba.length){
+                st1=0;
+            }
+            if(st2>=cir.length){
+                st2=0;
+            }
+        }
+        cir=aba;
+    }
+
+    // Start index becomes zero
+    public void resizeByLinearize(int newcapacity) {
+        Object aba[]=new Object[newcapacity];
+        int st2=start;
+
+        for(int i=0; i<=size; i++){
+            aba[i]=cir[st2];
+
+            st2++;
+
+            if(st2>=cir.length){
+                st2=0;
+            }
+        }
+        cir=aba;
+    }
+
+    /* pos --> position relative to start. Valid range of pos--> 0 to size.
+     * Increase array length by 3 if size==cir.length
+     * use resizeStartUnchanged() for resizing.
+     */
+    public void insertByRightShift(Object elem, int pos) {
+        if(size==cir.length){
+            resizeStartUnchanged(3+cir.length);
+        }
+        //System.out.println(cir.length);
+
+        int from=(start+size-1)%cir.length;
+
+        for(int i=from; i>=pos+start; i--){
+            if(i<0){
+                i=cir.length-1;
+            }
+            int to=(i+1)%cir.length;
+            cir[to]=cir[i];
+        }
+        //System.out.println(start+pos);
+        cir[(start+pos)]=elem;
+        size++;
+    }
+
+    public void insertByLeftShift(Object elem, int pos) {
+        if(size==cir.length){
+            resizeStartUnchanged(3+cir.length);
+        }
+        int from=start;
+        //int to=(from-1);
+        int nshift=size-pos;
+        for(int i=from; i<=pos+start; i++){
+            i=i%cir.length;
+            int to=(i-1);
+            if(to<0){
+                to=cir.length-1;
+            }
+            cir[to]=cir[i];
+        }
+        cir[start+pos]=elem;
+        size++;
+        start--;
+        //System.out.println(size);
+    }
+
+    /* parameter--> pos. pos --> position relative to start.
+     * Valid range of pos--> 0 to size-1
+     */
+    public void removeByLeftShift(int pos) {
+        int to=(start+pos)%cir.length;
+        int from=(to+1)%cir.length;
+        while(from!=start){
+            cir[to]=cir[from];
+            to=(to+1)%cir.length;
+            from=(from+1)%cir.length;
+        }
+        size--;
+    }
+
+    /* parameter--> pos. pos --> position relative to start.
+     * Valid range of pos--> 0 to size-1
+     */
+    public void removeByRightShift(int pos) {
+        int to=(start+pos)%cir.length;
+//        int from=to-1;
+//        if(from<0){
+//            from=cir.length-1;
+//        }
+        while(to!=start){
+            int from=to-1;
+            if(from<0){
+                from=cir.length-1;
+            }
+            cir[to]=cir[from];
+            to--;
+            if(to<0){
+                to=cir.length-1;
+            }
+        }
+        cir[start]=null;
+        start++;
+        size--;
+    }
+
+    //This method will check whether the array is palindrome or not
+    public void palindromeCheck() {
+        //TO DO
+    }
+
+    //This method will sort the values by keeping the start unchanged
+    public void sort() {
+        //TO DO
+    }
+
+    //This method will check the given array across the base array and if they are equivalent interms of values return true, or else return false
+    public boolean equivalent(CircularArray k) {
+        //TO DO
+        return false; // Remove this line
+    }
+}
